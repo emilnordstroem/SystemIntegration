5bc4804ca15924179830c62579cb6312032dabd2
emilnordstroem <emilstoeve@outlook.com>
2026-02-04 16:57:42 +0100

    Add project files.

---
+++ Lektion1RabbitMQSend/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Person
+{
+	public Person(string firstname, string lastname, int age, string email)
+	{
+		Firstname = firstname;
+		Lastname = lastname;
+		Age = age;
+		Email = email;
+	}
+
+	public string Firstname { get; set; }
+	public string Lastname { get; set; }
+	public int Age { get; set; }
+	public string Email { get; set; }
+
+	public override string ToString()
+	{
+		return $"\n{Firstname} {Lastname} ({Age})\nEmail: {Email}";
+	}
+
+}
---
+++ Lektion1RabbitMQSend/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+	queue: "hello",
+	durable: false,
+	exclusive: false,
+	autoDelete: false,
+	arguments: null
+	);
+
+/*
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+*/
+
+// Lektion 2 - Opgave 1
+var person = new Person("Emil", "Stoeve", 24, "emil@example.com");
+var json = JsonSerializer.Serialize(person);
+var body = Encoding.UTF8.GetBytes(json);
+
+
+await channel.BasicPublishAsync(
+	exchange: string.Empty,
+	routingKey: "hello",
+	body: body
+	);
+Console.WriteLine($" [x] Sent {json}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
